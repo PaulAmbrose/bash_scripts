59a1ce9224dd4d59db72172696c4e5ba0536fdbe
Paul Ambrose <pablo_rose34@hotmail.com>
2022-06-18 13:39:53 +0100

    Update git_backup_script.sh

--- crontab/github_backup/git_backup_script.sh
+++ crontab/github_backup/git_backup_script.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-declare -a StringArray=("https://github.com/PaulAmbrose/plan.git"_
-_"https://github.com/PaulAmbrose/bash_scripts.git"_
-_"https://github.com/PaulAmbrose/Learn_C_The_Hard_Way.git" "https://github.com/PaulAmbrose/shaw_Learn_C_The_Hard_Way.git"_
-_"https://github.com/PaulAmbrose/python.git", "https://github.com/PaulAmbrose/lambert-cards.git", "https://github.com/PaulAmbrose/PaulAmbrose.git" )
+declare -a StringArray=("https://github.com/PaulAmbrose/plan.git", "https://github.com/PaulAmbrose/bash_scripts.git", "https://github.com/PaulAmbrose/Learn_C_The_Hard_Way.git", "https://github.com/PaulAmbrose/shaw_Learn_C_The_Hard_Way.git", "https://github.com/PaulAmbrose/python.git", "https://github.com/PaulAmbrose/lambert-cards.git", "https://github.com/PaulAmbrose/PaulAmbrose.git" )
 
-
-
-
-
-
-
-
-# Iterate the string array using for loop
 for val in ${StringArray[@]}; do
    echo $val
 done
