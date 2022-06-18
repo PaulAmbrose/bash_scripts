81979249392b929899834d1e3fc59338be4e7914
Paul Ambrose <pablo_rose34@hotmail.com>
2022-06-18 13:33:47 +0100

    Update git_backup_script.sh

--- crontab/github_backup/git_backup_script.sh
+++ crontab/github_backup/git_backup_script.sh
@@ -1,1 +1,7 @@
-
+https://github.com/PaulAmbrose/plan.git
+https://github.com/PaulAmbrose/bash_scripts.git
+https://github.com/PaulAmbrose/Learn_C_The_Hard_Way.git
+https://github.com/PaulAmbrose/shaw_Learn_C_The_Hard_Way.git
+https://github.com/PaulAmbrose/python.git
+https://github.com/PaulAmbrose/lambert-cards.git
+https://github.com/PaulAmbrose/PaulAmbrose.git
